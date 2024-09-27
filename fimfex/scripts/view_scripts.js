47959a530c3af4338373561061ea6dfbe81db6b9
PseudoBob Delightus <pseudobob.delightus@gmail.com>
2024-09-26 23:08:06 -0400

    Add voting page navigation & layout

--- fimfex/scripts/view_scripts.js
+++ fimfex/scripts/view_scripts.js
@@ -113,6 +113,91 @@ async function submit_stories() {
     }
 }
 
+async function get_voting_submissions() {
+    const id = new URL(window.location.href).searchParams.get('id');
+    const username = document.getElementById('username').value;
+
+    const warningbox = document.getElementById('warningbox');
+    const resultbox = document.getElementById('resultbox');
+
+    try {
+        const res = await fetch(`http://127.0.0.1:7669/get-exchange/${id}?name=${username}`,{
+                method: 'GET'
+            }
+        );
+        const data = await res.json();
+
+        if (!res.ok) {
+            resultbox.innerHTML = ''
+            resultbox.classList.add('invisible')
+
+            warningbox.innerHTML = `
+                <p>The server returned an error:</p>
+                <p><strong>Error ${res.status}:</strong> ${data}</p> 
+            `
+            warningbox.classList.remove('invisible')
+            return;
+        }
+        else {
+            console.log(data);
+            draw_voting_submissions(data);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function transition(stage) {
+    const id = new URL(window.location.href).searchParams.get('id');
+    const passphrase = document.getElementById('passphrase').value;
+
+    switch (stage) {
+        case 'Submission': await goto_submission_stage(id, passphrase); break;
+        case 'Voting': await goto_voting_stage(id, passphrase); break;
+        case 'Selections': await goto_selections_stage(id, passphrase); break;
+        case 'Frozen': await goto_frozen_stage(id, passphrase); break;
+        default: break;
+    }
+}
+
+async function goto_submission_stage(id, passphrase) {
+    
+}
+
+async function goto_voting_stage(id, passphrase) {
+    try{
+        const res = await fetch(`http://127.0.0.1:7669/change-stage/${id}/${passphrase}?stage=Voting`,{
+            method: 'PATCH'
+        });
+        const data = await res.text();
+
+        if (!res.ok) {
+            warningbox.innerHTML = `
+                <p>The server returned an error:</p>
+                <p><strong>Error ${res.status}:</strong> ${data}</p> 
+            `
+            warningbox.classList.remove('invisible')
+            return;
+        }
+        else {
+            await get_exchange();
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+    console.log('Go to voting!!!')
+
+}
+
+async function goto_selections_stage(id, passphrase) {
+
+}
+
+async function goto_frozen_stage(id, passphrase) {
+
+}
+
 function build_view(data) {
     switch (data.stage) {
         case 'Submission': build_submission_view(data); break;
@@ -170,11 +255,70 @@ function build_submission_view(data) {
 
             <button id="submit_stories" onclick="submit_stories()">Submit Stories</button>
         </div>
+        <div>
+            <br>
+            <p>Admin controls:</p>
+            <label for="passphrase">passphrase:</label>
+            <input type="password" id="passphrase" name="passphrase">
+            <br>
+            <button id="goto_voting" onclick="transition('Voting')">Go to Voting stage</button>
+        </div>
     `
 }
 
 function build_voting_view(data) {
+    const viewbox = document.getElementById('viewbox');
+    viewbox.innerHTML = `
+        <h1>${data.title}</h1>
+        <p>This exchange is ready to accept votes.</p>
+        <p>At least one vote is required, but more is better.</p>
+        <div>
+            <div id="submission_voting_box">
+                <p>Submit your username before casting votes.<p>
+                <label for="username">username:</label>
+                <input type="text" id="username" name="username">
+                <button id="get_voting_submissions" onclick="get_voting_submissions()">Submit</button>
+            </div>
+        </div>
+        <div>
+            <br>
+            <p>Admin controls:</p>
+            <label for="passphrase">passphrase:</label>
+            <input type="password" id="passphrase" name="passphrase">
+            <br>
+            <button id="goto_submission" onclick="transition('Voting')">Go to Submission stage</button>
+            <button id="goto_selection" onclick="transition('Voting')">Go to Selection stage</button>
+        </div>
+    `
+}
 
+function draw_voting_submissions(data) {
+    const box = document.getElementById('submission_voting_box');
+    const username = document.getElementById('username').value;
+    
+    var string = 
+        `<p>Voting as: <span id=username>${username}</span></p>
+        <br>
+        <ul>`
+
+    data.submissions.forEach(submission => {
+        string += 
+            `\n<li class="vote_entry">
+            <p>`;
+        submission.stories.forEach(story => {
+            string += 
+                `${story}, `;
+        });
+        string += 
+            `</p>
+            <label>vote: </label><input type="number" name="vote">
+            </li>`;
+    });
+
+    string +=
+        '\n</ul>';
+    
+    box.innerHTML = string;
 }
 
 function build_selections_view(data) {
